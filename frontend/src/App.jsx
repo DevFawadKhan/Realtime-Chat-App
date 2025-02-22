785170bfbb2a12d94803202d105f38582b7a7844
DevFawadKhan <mfawadkhan05@gmail.com>
2025-02-22 03:24:31 -0700

    working on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-
+import Navbar from "./components/Navbar.jsx"
+import {Routes,Route} from 'react-router-dom'
+import HomePage from "./pages/HomePage.jsx"
+import SignUpPage from './pages/SignUpPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import SettingPage from './pages/SettingPage.jsx'
+import LogInPage  from './pages/LogInPage.jsx'
 function App() {
 
   return (
     <>
-  <h1>hello world</h1>
+   <Navbar/>
+<Routes>
+  <Route path="/" element={<HomePage/>}/>
+  <Route path="/login" element={<LogInPage/>}/>
+  <Route path="/signup" element={<SignUpPage/>}/>
+  <Route path="/setting" element={<SettingPage/>}/>
+  <Route path="/profile" element={<ProfilePage/>}/>
+</Routes>
     </>
   )
 }
